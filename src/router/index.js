cd20c368a02173b334170a5852d141522bab9b58
Jeremy Lerner <jeremy.quicklearner@gmail.com>
2018-08-04 19:21:38 -0700

    Added some test routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -10,6 +13,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
-  ]
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+  ],
+  mode: 'history'
 })
